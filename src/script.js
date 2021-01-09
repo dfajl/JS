1cdf6ad34225988b4a548f30fb003be7683a59eb
dfajl <dfajl@mail.ru>
2021-01-09 08:06:12 +0300

    homework1

---
+++ src/script.js
@@ -0,0 +1,48 @@
+'use strict';
+                                                            // ДОМАШНЯЯ РАБОТА
+
+// let money = prompt('Ваш бюджет на месяц?');
+// let time = prompt ('Введите дату в формате YYYY-MM-DD');
+
+// let appData = {
+//     budget: money,
+//     timeData: time,
+// 	expenses: {
+//         '7000':'1000'
+//     },
+// 	optionalExpenses: {},
+// 	income: [],
+// 	savings: false
+// };
+
+// let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+// 	a2 = prompt("Во сколько обойдется?"),
+// 	a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+//     a4 = prompt("Во сколько обойдется?",);
+    
+
+// appData.expenses.a1 = a2;
+// appData.expenses.a3 = a4;    
+
+// alert(appData.budget / 30);
+
+
+
+
+
+                                                        //  УСЛОВИЯ
+
+// if (2*4 == 9) {
+//     console.log ("Правильный ответ")
+// } else {
+//     console.log ("идиот")
+// }
+
+let num = 50;
+
+if (num < 49) {
+    console.log(true)
+
+}else {
+    console.log(false)
+}
