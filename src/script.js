1fcfb7cd986049f5755228ffb98a5333e7bed7e7
dfajl <dfajl@mail.ru>
2021-01-24 10:22:48 +0300

    js_train

--- src/script.js
+++ src/script.js
@@ -85,52 +85,130 @@
 
  
 
-let numb = 7 
+// let numb = 7 
                                           
-function showFirstMessage(text) {
-    alert(text);
-    numb = 90;
+// function showFirstMessage(text) {
+//     alert(text);
+//     numb = 90;
      
+// }
+
+// showFirstMessage('hello');
+
+// console.log(numb);
+
+
+
+
+
+
+
+// // внутри функции находится локальная переменная, так как ее не будет видно вне пределов функции
+
+// // showFirstMessage ('hello world');
+
+
+
+// // showFirstMessage () - вызов функции
+
+// // showFirstMessage - имя функции. записываем негласно так: глагол+то, над чем действие
+// // text - аргумент функции
+// // замыкание функции - это функция со всеми внешними переменными, которые ей доступны
+
+
+
+// console.log(calc (3, 4));
+// console.log(calc (6, 4));
+// console.log(calc (9, 4));
+
+
+// function calc (a,b) {
+//     return (a+b);
+// }
+
+
+
+// function retVar () {
+//     let num=50;
+//     return num;
+// }
+
+// let number = retVar();
+// console.log(number);
+
+
+                                            // Call back function
+
+
+// function first () {
+//     // какие-либо действия
+//     setTimeout ( function () {
+//         console.log(1);
+//     }, 500);
+    
+// }
+
+// function second () {
+//    console.log(2);
+// }
+
+// first ();
+// second ();
+
+
+// function learnJS (lang, callback) {
+//     console.log ("я учу  " + lang);
+//     callback ();
+// }
+
+// learnJS ("JavaScript", function() {
+//     console.log ('i have learned the third lesson');
+// });
+// это был пример вызова функции, как аргумента от другой функции
+
+function learnJS (lang, callback) {
+    console.log ("я учу  " + lang);
+    callback ();  
+};
+
+function done () {
+    console.log(1);
+};
+
+
+learnJS ("JavaScript", done);
+
+
+
+                                                //   Объекты
+
+let options = {
+    width: 1024,
+    height: 1023,
+    name: 'test'
+};
+
+console.log (options.height); 
+// вывел значение свойства height из объекта
+
+options.bool = false;
+// добавил свойство bool со знчением false в объект
+
+options.colors = {
+    color1: 'red',
+    color2: 'white',
+    color3: 'green'
+};
+// добавил  объект colors в имеющийся объект options
+
+delete options.name;
+// удалили выбранное значение из объекта
+
+console.log(options);
+
+for (let key in options) {
+    console.log ('propery ' + key + ' has a value ' + options[key]);
 }
+// с помощью задания такого цикла и переменной в нем "key" мы выводим все свойства из объекта по очереди
 
-showFirstMessage('hello');
-
-console.log(numb);
-
-
-
-
-
-
-// внутри функции находится локальная переменная, так как ее не будет видно вне пределов функции
-
-// showFirstMessage ('hello world');
-
-
-
-// showFirstMessage () - вызов функции
-
-// showFirstMessage - имя функции. записываем негласно так: глагол+то, над чем действие
-// text - аргумент функции
-// замыкание функции - это функция со всеми внешними переменными, которые ей доступны
-
-
-
-console.log(calc (3, 4));
-console.log(calc (6, 4));
-console.log(calc (9, 4));
-
-
-function calc (a,b) {
-    return (a+b);
-}
-
-
-
-function retVar () {
-    let num=50;
-    return num;
-}
-
-let number = retVar();
-console.log(number);
+console.log (Object.keys(options).length);
